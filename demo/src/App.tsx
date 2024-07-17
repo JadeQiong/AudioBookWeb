59fbb1e04edda2b74f06577e40354c14be827894
shannonmeng333 <shannon.meng@eventeny.com>
2024-07-16 20:56:47 -0400

    merged styling

--- demo/src/App.tsx
+++ demo/src/App.tsx
@@ -166,27 +166,52 @@ function App() {
           </Stack>
         </Stack>
        
-<Stack sx={{height: '120px', backgroundColor: 'red'}}></Stack>
+<Stack sx={{height: '120px'}}></Stack>
 <ContinuousSlider audio={curAudio}></ContinuousSlider>
+      
+<Stack sx={{height: '50px'}}></Stack>
+          <div className="container_background">
+          <Stack direction="column" sx={{ width: '60%', marginBottom: 5 }}>
+          <Typography variant="h4" component="h1" gutterBottom sx={{
+            fontWeight: 'bold',
+            background: 'linear-gradient(to bottom, #ebebec, #979797)',
+            WebkitBackgroundClip: 'text',
+            WebkitTextFillColor: 'transparent',
+            display: 'inline'
+          }}>
+              Engaging, Captivating, Premium
+            </Typography>
+            <Typography variant="h6" gutterBottom sx={{ 
+              fontWeight: 'bold',
+              background: '#c1c1c1',
+              WebkitBackgroundClip: 'text',
+              WebkitTextFillColor: 'transparent',
+              display: 'inline'
+            }}>
+              AI-Powered Podcasts on Today's Best Reads
+            </Typography>
+            <Typography variant="body1" gutterBottom>
+              No more dry summaries—AI-Powered BookTalks delivers a vibrant audio
+              experience that brings literature to life, connecting readers with
+              books in a profound and modern way. Experience the future of reading
+              with AI-Powered BookTalks, where every book becomes a captivating
+              conversation.
+            </Typography>
 
-        <Stack direction="column" sx={{ width: '60%', margin: 5 }}>
-          <Typography variant="h4" gutterBottom>
-            Engaging, Captivating, Premium
-          </Typography>
-          <Typography variant="body1" gutterBottom>
-            AI-Powered Podcasts on Today's Best Reads
-          </Typography>
-          <Typography variant="body2" gutterBottom>
-            No more dry summaries—AI-Powered BookTalks delivers a vibrant audio
-            experience that brings literature to life, connecting readers with
-            books in a profound and modern way. Experience the future of reading
-            with AI-Powered BookTalks, where every book becomes a captivating
-            conversation.
-          </Typography>
-          <Button size="small">Join Our Waitlist</Button>
-        </Stack>
+            <div className="container">
+            <div className="border-box">
+              <div className="content">
+                Join Our Waitlist
+              </div>
+            </div>
+            </div>
+          </Stack>
+          </div>
+
+        </div>
+
       </div>
-    </div>
+    // </div>
   );
 }
 
